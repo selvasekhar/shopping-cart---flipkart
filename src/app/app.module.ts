66a78fe11497b8be23f6857e2ebbb199ab76c887
selvasekhar <selvasekhar@hotmail.com>
2022-12-28 20:41:42 +0530

    shopping-cart-flipkart
    shopping-cart-flipkart

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductsComponent } from './components/products/products.component';
+// import { ApiComponent } from './service/api/api.component';
+import {HttpClientModule} from '@angular/common/http'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CartComponent,
+    ProductsComponent,
+    
+    // ApiComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
